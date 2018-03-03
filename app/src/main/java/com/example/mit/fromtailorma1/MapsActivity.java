6d50345eb94c925895e983ef9da6b9076185fdd4
Mithun <kl13lifestyle@gmail.com>
2018-03-03 17:12:10 +0530

    first commit

--- app/src/main/java/com/example/mit/fromtailorma1/MapsActivity.java
+++ app/src/main/java/com/example/mit/fromtailorma1/MapsActivity.java
@@ -1,7 +1,9 @@
 package com.example.mit.fromtailorma1;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -9,15 +11,24 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -26,32 +37,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationClient;
+    PlaceAutocompleteFragment placeAutoComplete;
+    //geofence
+    //private GeofencingClient mGeofencingClient;
+    //mGeofencingClient = LocationServices.getGeofencingClient(this);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
+
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        placeAutoComplete = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.place_autocomplete);
+        placeAutoComplete.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                addMarker(place);
+                Log.d("Maps", "Place selected: " + place.getName());
+            }
+
+            @Override
+            public void onError(Status status) {
+
+                Log.d("Maps", "An error occurred: " + status);
+            }
+        });
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+
+
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
         {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
@@ -68,10 +94,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             // Add a marker in Sydney and move the camera
                             LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
                             float zoomLevel = (float) 16.0; //This goes up to 21
+                            //addMarker(currentLocation);
                             mMap.addMarker(new MarkerOptions().position(currentLocation).title("My Location"));
                             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,zoomLevel));
                         } else {
-                            Log.v("Test Log", "location is null");
+                            //Log.v("Test Log", "location is null");
                             String msg = "Location unknown";
                             Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
                         }
@@ -81,14 +108,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .addOnFailureListener(this, new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.v("Test Log", "Some error getting gps");
-                        Log.d("MapDemoActivity", "Error trying to get last GPS location");
+                        //Log.v("Test Log", "Some error getting gps");
+                        //Log.d("MapDemoActivity", "Error trying to get last GPS location");
                         String msg = "Error trying to get last GPS location";
                         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
                         e.printStackTrace();
                     }
                 });
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Toast.makeText(getApplicationContext(), "YOU CLICKED ON " + marker.getTitle(), Toast.LENGTH_LONG).show();
+                CircleOptions circleOptions = new CircleOptions()
+                        .center( new LatLng(marker.getPosition().latitude, marker.getPosition().longitude) )
+                        .radius(100)
+                        .fillColor(0x40ff0000)
+                        .strokeColor(Color.TRANSPARENT)
+                        .strokeWidth(2);
+
+// Get back the mutable Circle
+                Circle circle = mMap.addCircle(circleOptions);
+// more operations on the circle...
+                return false;
+            }
+        });
      }
+
+
+
+    public void addMarker(Place p){
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(p.getLatLng());
+        markerOptions.title(p.getName()+"");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+        //mMap.setOnMarkerClickListener(this);
+        mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(p.getLatLng()));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(13));
+    }
+
+
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
@@ -102,6 +162,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             | View.SYSTEM_UI_FLAG_FULLSCREEN
                             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
+    }
+
+    public void backToHome(View view) {
+        startActivity(new Intent(MapsActivity.this, MainActivity.class));
+        // close maps activity
+        finish();
     }
 
 }
